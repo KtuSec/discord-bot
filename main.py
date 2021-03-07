6cb33d4fbef5f226b9898c98e4a025fec22506b5
mucahiddogan <mucahid.dogan@ceng.ktu.edu.tr>
2021-03-07 05:02:49 +0300

    gitignore and timer added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext.commands import Bot
+import datetime
+import time
+from tokenid import *
 
-token = "" # discord development kanalinda
+token = token # discord development kanalinda
 
 client = discord.Client()
 bot = Bot(command_prefix=".")
@@ -9,6 +12,20 @@ bot = Bot(command_prefix=".")
 @bot.event
 async def on_ready():
         print("bot is running")
+        channel = bot.get_channel(development_channel_id)
+        timenow = str(datetime.datetime.now())
+        while True:
+            timenow = str(datetime.datetime.now())
+            if timenow[0:19] == "2021-03-07 04:59:30":
+                await channel.send("8 MART DÜNYA KADINLAR GÜNÜMÜZ KUTLU OLSUN @everyone")
+                break
+            else:
+                print(datetime.datetime.now())
+                # time.sleep(0.1)
+            time.sleep(0.01)
+
+        # await channel.send('hello')
+
 @bot.event
 async def on_message(message):
     if message.author.bot:
